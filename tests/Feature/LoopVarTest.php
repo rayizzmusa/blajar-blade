51ca577d35033a55097d9e840ada5eb616c26d52
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-10-03 20:48:34 +0700

    loop variable

---
+++ tests/Feature/LoopVarTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoopVarTest extends TestCase
+{
+    public function testLoopVar()
+    {
+        $this->view("loopvariable", ["hobbies" => ["makan", "tidur", "begadang"]])
+            ->assertSeeText("1")
+            ->assertSeeText("2")
+            ->assertSeeText("makan");
+    }
+}
