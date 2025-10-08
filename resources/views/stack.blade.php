512f76a0c530d158b3a63f21b45da5f38a406038
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-10-08 21:14:55 +0700

    stack

---
+++ resources/views/stack.blade.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+    @push("script")
+    <script src="first.js"></script>
+    @endpush
+
+    @push("script")
+    <script src="second.js"></script>
+    @endpush
+
+    @prepend("script")
+    <script src="third.js"></script>ip
+    @endprepend
+
+    @stack("script")
+</body>
+
+</html>
